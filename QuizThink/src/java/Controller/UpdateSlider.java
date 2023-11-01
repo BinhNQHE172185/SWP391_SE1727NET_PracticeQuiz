207b83cacf1ec95c730d4509fc83c8d804ed5f84
kiennmhe171466 <144405697+kiennmhe171466@users.noreply.github.com>
2023-11-01 23:19:30 +0700

    Create UpdateSlider.java

---
+++ QuizThink/src/java/Controller/UpdateSlider.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import DAO.SliderDAO;
+import Model.Slider;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author minhk
+ */
+@WebServlet(name = "UpdateSlider", urlPatterns = {"/updateslider"})
+public class UpdateSlider extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String sliderId = request.getParameter("sliderId");
+        int sliderid = Integer.valueOf(sliderId);
+        String title = request.getParameter("title");
+        String name = request.getParameter("name");
+        String imageURL = request.getParameter("imageURL");
+        String description = request.getParameter("description");
+        SliderDAO sliderDAO = new SliderDAO();
+        sliderDAO.updateSlider(title, name, imageURL, description, sliderid);
+          Slider slider = sliderDAO.getSliderById(sliderid);
+        request.setAttribute("slider", slider);
+        request.getRequestDispatcher("EditSlider.jsp").include(request, response);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
