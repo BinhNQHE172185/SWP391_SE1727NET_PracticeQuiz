b1897a39b539c6f90215e27cb95b38a1291f62db
DungBNHE171450 <144671027+DungBNHE171450@users.noreply.github.com>
2023-11-08 22:06:54 +0700

    check out

---
+++ QuizThink/src/java/Controller/ProcessCheckoutServlet.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import DAO.TransactionDAO;
+import Model.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name = "ProcessCheckoutServlet", urlPatterns = {"/ProcessCheckout"})
+public class ProcessCheckoutServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        Account currUser = (Account) session.getAttribute("currUser");
+        String fullname = request.getParameter("fullname");
+        String email = request.getParameter("email");
+        String payMethod = request.getParameter("paymentMethod");
+        String nameOnCard = request.getParameter("nameOnCard");
+        String creditNumber = request.getParameter("creditNumber");
+        String expirationDate = request.getParameter("expiration");
+        String cvv = request.getParameter("cvv");
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        LocalDateTime currentTime = LocalDateTime.now();
+        Date transactionDate = Date.valueOf(currentTime.toLocalDate()); 
+        Date expiration = Date.valueOf(expirationDate);
+        
+        String exp = sdf.format(expiration);
+        Date expi =Date.valueOf(exp);
+        
+        TransactionDAO dao = new TransactionDAO();
+        if(dao.isValidFullName(fullname) != true || dao.isValidFullName(nameOnCard) != true){
+            String mess = "Invalid name" ;
+            request.setAttribute("mess", mess);
+            request.getRequestDispatcher("CheckOut.jsp").include(request, response);
+        }
+        if(dao.isValidCreditCard(creditNumber) != true){
+            String mess = "Invalid credit card number. Make sure you input correct credit number!" ;
+            request.setAttribute("mess", mess);
+            request.getRequestDispatcher("CheckOut.jsp").include(request, response);
+        }
+        if(transactionDate.after(expiration)){
+            String mess = "Your card has expired";
+            request.setAttribute("mess", mess);
+            request.getRequestDispatcher("CheckOut.jsp").include(request, response);
+        }
+        if(dao.isValidCVV(cvv)){
+            String mess = "Invalid cvv. Make sure you input correct cvv number!" ;
+            request.setAttribute("mess", mess);
+            request.getRequestDispatcher("CheckOut.jsp").include(request, response);
+        }
+        
+        dao.addTransaction(currUser.getAccountId(), 1, transactionDate, fullname, email, payMethod, nameOnCard, creditNumber, expi, cvv, 20);
+        response.sendRedirect("ThankYou.jsp");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ QuizThink/src/java/DAO/TransactionDAO.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import DAL.DBContext;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author admin
+ */
+public class TransactionDAO extends DBContext {
+
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    public static boolean isValidFullName(String fullName) {
+        // Define a regular expression pattern for a full name with multiple surnames
+        String regex = "^[A-Z][a-z]*(\\s[A-Z][a-z]*)+$";
+
+        // Compile the regular expression pattern
+        Pattern pattern = Pattern.compile(regex);
+
+        // Create a Matcher object
+        Matcher matcher = pattern.matcher(fullName);
+
+        // Check if the input matches the pattern
+        return matcher.matches();
+    }
+
+    public static boolean isValidCreditCard(String number) {
+        int[] digits = new int[number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            digits[i] = Character.getNumericValue(number.charAt(i));
+        }
+
+        for (int i = digits.length - 2; i >= 0; i -= 2) {
+            int doubleDigit = digits[i] * 2;
+            if (doubleDigit > 9) {
+                doubleDigit -= 9;
+            }
+            digits[i] = doubleDigit;
+        }
+
+        int sum = 0;
+        for (int digit : digits) {
+            sum += digit;
+        }
+
+        return (sum % 10) == 0;
+    }
+
+    public static boolean isValidCVV(String cvv) {
+        if (cvv == null || (cvv.length() != 3 && cvv.length() != 4)) {
+            return false;
+        }
+        for (int i = 0; i < cvv.length(); i++) {
+            if (!Character.isDigit(cvv.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true; // CVV is valid.
+    }
+
+    public void addTransaction(int accID, int memID, Date tranDate, String fullname, String email, String payMethod, String nameOnCard, String creditNumber, Date expiration, String cvv, float totalMoney) {
+        try {
+            String query = " insert into [Transaction]\n"
+                    + "(Account_id, Membership_id, Transaction_date, Fullname, Email, Payment_method, Name_on_card, Card_credit_number, Expiration, CVV, Total_money)\n"
+                    + "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+             ps = getConnection().prepareStatement(query);
+             ps.setInt(1, accID);
+             ps.setInt(2, memID);
+             ps.setDate(3, tranDate);
+             ps.setString(4, fullname);
+             ps.setString(5, email);
+             ps.setString(6, payMethod);
+             ps.setString(7, nameOnCard);
+             ps.setString(8, creditNumber);
+             ps.setDate(9, expiration);
+             ps.setString(10, cvv);
+             ps.setFloat(11, totalMoney);
+             ps.executeQuery();
+        } catch (Exception e) {
+
+        }
+    }
+    
+    public void updateAccountRole(int accID){
+        
+    }
+
+    public static void main(String[] args) {
+        String name = "Dung Bui Ngoc";
+        String cardNumber = "1234567890123456";
+        String cvv = "1232";
+        TransactionDAO dao = new TransactionDAO();
+        boolean isName = dao.isValidFullName(name);
+        System.out.println(isName);
+        boolean isCard = isValidCreditCard(cardNumber);
+        System.out.println(isCard);
+        String creditCardNumber = "1234567890123452";
+        if (isValidCreditCard(creditCardNumber)) {
+            System.out.println("Valid credit card number");
+        } else {
+            System.out.println("Invalid credit card number");
+        }
+        System.out.println(isValidCVV(cvv));
+    }
+}
